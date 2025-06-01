3f2980a96c156dd13a60a6ce4841956d9c3e274f
Tatyana <filippovat42@gmail.com>
2025-06-01 10:33:54 +0300

    подключение cms

--- src/app/detailedPage/page.tsx
+++ src/app/detailedPage/page.tsx
@@ -2,9 +2,11 @@ import Header from "@/components/header/Header";
 import HomeBanner from "@/app/detailedPage/homeBanner/HomeBanner";
 import ProductList from "@/app/detailedPage/productList/ProductList";
 import Footer from "@/components/footer/Footer";
+import { CMS_URL } from "@/constant";
 
 export default async function Home() {
-  const data = await fetch("http://localhost:1337/api/facades?populate=*");
+  const data = await fetch(CMS_URL + "/api/facades?populate=*");
+  console.log(data);
   const posts = await data.json();
   return (
     <div>
