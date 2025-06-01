5565ae99207f817b16c6e43f976b68e5154c1311
ivandmitrackov <dmitrachkovivan@yandex.ru>
2025-06-01 09:25:49 +0300

    Добавил CICD

--- src/app/detailedPage/page.tsx
+++ src/app/detailedPage/page.tsx
@@ -1,16 +1,16 @@
 import Header from "@/components/header/Header";
 import HomeBanner from "@/app/detailedPage/homeBanner/HomeBanner";
-import ProductList from "@/app/detailedPage/productList/ProductList";
+// import ProductList from "@/app/detailedPage/productList/ProductList";
 import Footer from "@/components/footer/Footer";
 
 export default async function Home() {
-  const data = await fetch("http://localhost:1337/api/facades?populate=*");
-  const posts = await data.json();
+  // const data = await fetch("http://localhost:1337/api/facades?populate=*");
+  // const posts = await data.json();
   return (
     <div>
       <Header />
       <HomeBanner />
-      <ProductList data={posts.data} />
+      {/*<ProductList data={posts.data} />*/}
       <Footer />
     </div>
   );
